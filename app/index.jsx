25efc9976f2ee70739078ad7a01721d28130ab27
Carlos23806 <cm2873640@gmail.com>
2024-08-21 16:35:53 -1000

    min: 1h 2min

---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import {Image, Text, View, ScrollView } from 'react-native';
+import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+          <ScrollView contentContainerStyle={{ height: '100%'}}>
+          <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMod="contain"
+          />
+
+          <Image 
+            source={images.cards}
+            className="max-w--[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless possibilities with {' '}
+              <Text className="text-secondary-200 ">Aora_Adso</Text>
+            </Text>
+
+            <Image 
+              source={images.path}
+              className="w-[136px] h-[15px] adsolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora_Adso </Text>
+            <customButton/> 
+          
+          </View>
+          </ScrollView>
+    </SafeAreaView>
+  );
+} 
+
